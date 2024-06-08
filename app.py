6e6e36078c548ae323fc4e6ec495c6d4f86f7b07
n.arun_menon@student.unsw.edu.au <z5417727@ad.unsw.edu.au>
2024-06-08 19:36:14 +1000

    a solid reommender system working now

--- app.py
+++ app.py
@@ -1,16 +1,7 @@
-import pandas as pd
 # import julep
-from textblob import TextBlob
 # from julep import AsyncClient
 # -> later used for data extraction from websites
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-import article_analysis
-import music_analysis
 import recommender
-
-# TODO: csv file example from where music is from
-response_music = pd.read_csv('data/tcc_ceds_music.csv')
 
 # # TODO: using Julep AI
 # JULEP_API_KEY = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI3NTcyM2I5Yy04MTgxLTQwOTMtODhhMC1hMjE2NzdhYWFjNzYiLCJlbWFpbCI6Im5pcmFhcnVubWVub25AZ21haWwuY29tIiwiaWF0IjoxNzE3ODIwMzIyLCJleHBpcmVzSW4iOiIxeSIsInJhdGVMaW1pdFBlck1pbnV0ZSI6MzUwMCwicXVvdGFSZXNldCI6IjFoIiwiY2xpZW50RW52aXJvbm1lbnQiOiJzZXJ2ZXIiLCJzZXJ2ZXJFbnZpcm9ubWVudCI6InByb2R1Y3Rpb24iLCJ2ZXJzaW9uIjoidjAuMiIsImV4cCI6MTc0OTM3NzkyMn0._JIGxAoEiZ7VpypUcGLbClSVumwK_mumRBJ_eitfwaCQzY-V9_bkIkh_HkjRd7KV6SHNiIWQRjckWGCa5OWtIA"
@@ -24,11 +15,9 @@ response_music = pd.read_csv('data/tcc_ceds_music.csv')
 # # Analysing the article first
 # example = article_analysis.relevant_info("https://www.opencolleges.edu.au/blogs/articles/our-best-ever-study-playlist")
 example = "https://www.opencolleges.edu.au/blogs/articles/our-best-ever-study-playlist"
-# # get the music so we can analyse that too
-# music = music_analysis.data()
-# example: 
-recommender.recommend_music(example)
-
-print(example)
+# getting the music that was recommended based on the url
+music = recommender.recommend_music(example)
+# TODO: printing out the results
+print(music)
 
 
--- recommender.py
+++ recommender.py
@@ -22,17 +22,17 @@ def calculate_similarity(article_tokens, music_tokens):
 def recommend_music(article_url, music_data=response_music):
     recommendations = []
     # extracting features of the article
-    article_sentiments, article_content, article_themes = article_analysis.relevant_info(article_url)
+    (article_polarity, article_sentiment), article_content, article_themes = article_analysis.relevant_info(article_url)
     # for each lyric
     for idx, row in music_data.iterrows():
         lyrics = row['lyrics']
-        music_tokens, music_freq_dist, music_sentiment_scores = music_analysis.extract_features_from_lyrics(lyrics)
+        music_tokens, music_freq_dist, (music_polarity, music_sentiment) = music_analysis.extract_features_from_lyrics(lyrics)
         
         # Calculate similarity score
         similarity_score = calculate_similarity(article_content, music_tokens)
         
         # Compare sentiment scores
-        sentiment_difference = abs(article_sentiments[0] - music_sentiment_scores['compound'])
+        sentiment_difference = abs(article_polarity - music_sentiment)
         
         # Combine scores (e.g., weighted sum)
         combined_score = (similarity_score * 0.7) - (sentiment_difference * 0.3)
@@ -42,7 +42,7 @@ def recommend_music(article_url, music_data=response_music):
     # Sort recommendations by combined score
     recommendations.sort(key=lambda x: x[2], reverse=True)
     
-    return recommendations[:10]  # Return top 10 recommendations
+    return recommendations[:5]  # Return top 5 recommendations
 
 
 
